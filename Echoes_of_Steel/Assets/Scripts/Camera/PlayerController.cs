263e0f48d18d45b5afa3966b05adbf6cd348b2a8
Tohwa | Sascha <Tohwa.gaming@gmail.com>
2024-05-29 15:23:27 +0200

    testet camera controller on primitive player controller | Still using old input!

--- Echoes_of_Steel/Assets/Scripts/Camera/PlayerController.cs
+++ Echoes_of_Steel/Assets/Scripts/Camera/PlayerController.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private Rigidbody rb;
+    [SerializeField]
+    private float moveSpeed;
+
+    private bool forward;
+    private bool left;
+    private bool right;
+    private bool backward;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(forward)
+        {
+            rb.velocity += Vector3.forward * moveSpeed * Time.deltaTime;
+        }
+
+        if(left)
+        {
+            rb.velocity += Vector3.left * moveSpeed * Time.deltaTime;
+        }
+
+        if(right)
+        {
+            rb.velocity += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+
+        if(backward)
+        {
+            rb.velocity += Vector3.back * moveSpeed * Time.deltaTime;
+        }
+
+        UserInput();
+    }
+
+    void UserInput()
+    {
+        forward = Input.GetKey(KeyCode.W);
+        right = Input.GetKey(KeyCode.D);
+        left = Input.GetKey(KeyCode.A);
+        backward = Input.GetKey(KeyCode.S);
     }
 }
