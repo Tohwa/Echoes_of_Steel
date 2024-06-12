f3e4c540991ce461f2258b13699fd49ac1c99a9d
Daft <lantow79@gmail.com>
2024-06-12 09:41:11 +0200

    [ADDED] Grappling Hook Starter, Updated Camera Script

--- Echoes_of_Steel/Assets/Scripts/Player/PController.cs
+++ Echoes_of_Steel/Assets/Scripts/Player/PController.cs
@@ -11,6 +11,7 @@ public class PController : MonoBehaviour
     public InputAction jump;
     public InputAction dash;
     public InputAction shooting;
+    public InputAction grapplingHook;
 
     [Header("Framework Variables")]
     public Camera mainCamera;
@@ -23,6 +24,7 @@ public class PController : MonoBehaviour
 
     [Header("Simple Move and Jump Variables")]
     public float moveSpeed = 5f;
+    public float rotationSpeed = 10f;
     public float jumpForce = 5f;
     public float maxJumps = 2f;
     public float curJumps;
@@ -43,7 +45,100 @@ public class PController : MonoBehaviour
     public float weaponDamage;
     public Transform bulletSpawn;
     private Transform pCamera;
+
+    [Header("Grapple Variables")]
+    public float maxGrappleDistance;
+    public float grappleDelayTime;
+    public float grapplingCd;
+    private float grapplingCdTimer;
+    public float overshootYAxis;
+    private bool grappling;
+    private bool grappleFreeze;
+    private bool activeGrapple;
+    public LayerMask whatIsGrappable;
+    private Vector3 velocityToSet;
+    public Transform gunTip;
+    public LineRenderer lr;
+
+
+    private Vector3 grapplePoint;
     #endregion
+
+
+    private void OnGrappleInput(InputAction.CallbackContext context)
+    {
+        if (grapplingCdTimer > 0) return;
+
+        grappling = true;
+
+        grappleFreeze = true;
+
+        RaycastHit hit;
+        if (Physics.Raycast(gunTip.position, gunTip.forward, out hit, maxGrappleDistance, whatIsGrappable))
+        {
+            grapplePoint = hit.point;
+            Invoke(nameof(ExecuteGrapple), grappleDelayTime);
+        }
+        else
+        {
+            grapplePoint = gunTip.position + gunTip.forward * maxGrappleDistance;
+            Invoke(nameof(StopGrapple), grappleDelayTime);
+        }
+
+        lr.enabled = true;
+        lr.SetPosition(1, grapplePoint);
+    }
+    private void ExecuteGrapple()
+    {
+        rb.velocity = Vector3.zero;
+
+        Vector3 lowestPoint = new Vector3(transform.position.x, transform.position.y - 1f, transform.position.z);
+
+        float grapplePointRelativeYPos = grapplePoint.y - lowestPoint.y;
+        float highestPointOnArc = grapplePointRelativeYPos + overshootYAxis;
+
+        if (grapplePointRelativeYPos < 0)
+        {
+            highestPointOnArc = overshootYAxis;
+        }
+
+        JumpToPosition(grapplePoint, highestPointOnArc);
+
+        Invoke(nameof(StopGrapple), 1f);
+    }
+    private void StopGrapple()
+    {
+        grappling = false;
+
+        grappleFreeze = false;
+
+        grapplingCdTimer = grapplingCd;
+
+        lr.enabled = false;
+    }
+    public void JumpToPosition(Vector3 targetPosition, float trajectoryHeight)
+    {
+        activeGrapple = true;
+
+        velocityToSet = CalculateGrappleJumpVelocity(transform.position, targetPosition, trajectoryHeight);
+        Invoke(nameof(SetVelocity), 0.1f);
+    }
+    private void SetVelocity()
+    {
+        rb.velocity = velocityToSet;
+    }
+    public Vector3 CalculateGrappleJumpVelocity(Vector3 startPoint, Vector3 endPoint, float trajectoryHeight)
+    {
+        float gravity = Physics.gravity.y;
+        float displacementY = endPoint.y - startPoint.y;
+        Vector3 displacementXZ = new Vector3(endPoint.x - startPoint.x, 0f, endPoint.z - startPoint.z);
+
+        Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * gravity * trajectoryHeight);
+        Vector3 velocityXZ = displacementXZ / (Mathf.Sqrt(-2 * trajectoryHeight / gravity)
+            + Mathf.Sqrt(2 * (displacementY - trajectoryHeight) / gravity));
+
+        return velocityXZ + velocityY;
+    }
 
     private void Start()
     {
@@ -52,28 +147,23 @@ public class PController : MonoBehaviour
         mainCamera = Camera.main;
         pCamera = Camera.main.transform;
     }
-
     private void Update()
     {
         GroundCheck();
         isMoving = moveInput != Vector2.zero;
 
-        // Handle shooting
-        if (automatic)
+        if (grapplingCdTimer > 0)
         {
-            if (shooting.ReadValue<float>() > 0.1f && Time.time >= lastFireTime + fireCooldown)
-            {
-                lastFireTime = Time.time;
-                Shoot();
-            }
+            grapplingCdTimer -= Time.deltaTime;
         }
-        else if (shooting.triggered && Time.time >= lastFireTime + fireCooldown)
-        {
-            lastFireTime = Time.time;
-            Shoot();
-        }
-    }
 
+        //if (grappleFreeze)
+        //{
+        //rb.velocity = Vector3.zero;
+        //}
+
+        WeaponHandler();
+    }
     private void FixedUpdate()
     {
         if (isDashing)
@@ -85,34 +175,42 @@ public class PController : MonoBehaviour
             Move();
         }
     }
-
+    private void LateUpdate()
+    {
+        if (grappling)
+        {
+            lr.SetPosition(0, gunTip.position);
+        }
+    }
     void OnEnable()
     {
         movement.Enable();
         jump.Enable();
         dash.Enable();
         shooting.Enable();
+        grapplingHook.Enable();
 
         jump.performed += OnJumpInput;
         movement.performed += OnMovementInput;
         movement.canceled += OnMovementInput;
         dash.performed += OnDashInput;
-        shooting.performed += OnShootingInput;
+        grapplingHook.performed += OnGrappleInput;
     }
-
     void OnDisable()
     {
         movement.Disable();
         jump.Disable();
         dash.Disable();
         shooting.Disable();
+        grapplingHook.Disable();
 
         movement.performed -= OnMovementInput;
         movement.canceled -= OnMovementInput;
         jump.performed -= OnJumpInput;
         dash.performed -= OnDashInput;
-        shooting.performed -= OnShootingInput;
+        grapplingHook.performed -= OnGrappleInput;
     }
+
 
     private void OnMovementInput(InputAction.CallbackContext context)
     {
@@ -131,6 +229,12 @@ public class PController : MonoBehaviour
 
         Vector3 movementVector = forward * moveInput.y + right * moveInput.x;
         movementVector *= moveSpeed * Time.fixedDeltaTime;
+
+        if (movementVector != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(movementVector);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.fixedDeltaTime * rotationSpeed);
+        }
 
         rb.MovePosition(rb.position + movementVector);
     }
@@ -181,9 +285,21 @@ public class PController : MonoBehaviour
         }
     }
 
-    private void OnShootingInput(InputAction.CallbackContext context)
+    private void WeaponHandler()
     {
-        
+        if (automatic)
+        {
+            if (shooting.ReadValue<float>() > 0.1f && Time.time >= lastFireTime + fireCooldown)
+            {
+                lastFireTime = Time.time;
+                Shoot();
+            }
+        }
+        else if (shooting.triggered && Time.time >= lastFireTime + fireCooldown)
+        {
+            lastFireTime = Time.time;
+            Shoot();
+        }
     }
     private void Shoot()
     {
