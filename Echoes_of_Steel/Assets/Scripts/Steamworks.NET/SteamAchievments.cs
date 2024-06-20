579442238cceccd94cd6a22058c38a904a81d3cd
Tohwa | Sascha <Tohwa.gaming@gmail.com>
2024-06-21 00:58:32 +0200

    [ADDED] All Steam Achievments now properly integradted again

--- Echoes_of_Steel/Assets/Scripts/Steamworks.NET/SteamAchievments.cs
+++ Echoes_of_Steel/Assets/Scripts/Steamworks.NET/SteamAchievments.cs
@@ -6,25 +6,12 @@ using UnityEngine;
 
 public class SteamAchievment : MonoBehaviour
 {
-    [SerializeField] InteractionStatus interactionStatus;
-
     void Start()
     {
         if (!SteamManager.Initialized)
         {
             return;
         }
-    }
-
-    private void Update()
-    {
-        if (interactionStatus.HasInteracted("Memory #1") && interactionStatus.HasInteracted("Memory #2"))
-        {
-            SteamUserStats.SetAchievement("ACH_WIN_ONE_GAME");
-        }
-
-        UpdateSteamStats();
-
     }
 
     private void UpdateSteamStats()
