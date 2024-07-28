e0f4b072fe3b9a7fbd59d57ada35086841bd034d
Tohwa | Sascha <Tohwa.gaming@gmail.com>
2024-07-28 04:48:20 +0200

    [CHANGED] edited cutscene script | seems to be kind of working need proper cutscenes for it

--- Echoes_of_Steel/Assets/Scripts/Cutscenes/TimeLineDirector.cs
+++ Echoes_of_Steel/Assets/Scripts/Cutscenes/TimeLineDirector.cs
@@ -5,36 +5,63 @@ using UnityEngine.Playables;
 
 public class TimeLineDirector : MonoBehaviour
 {
+    [SerializeField] private GameObject m_Player;
+    [SerializeField] private GameObject m_Camera;
+
+    [SerializeField] private GameObject m_goodEnding;
+    [SerializeField] private GameObject m_badEnding;
+
     [SerializeField] private PlayableDirector m_goodDirector;
     [SerializeField] private PlayableDirector m_badDirector;
 
     [SerializeField] private float m_waitDureation = 5f;
 
+    private void Start()
+    {
+        m_goodEnding = gameObject.transform.GetChild(0).gameObject;
+        m_badEnding = gameObject.transform.GetChild(1).gameObject;
+    }
+
     private void CheckCorruptionMeter()
     {
+        m_Player = GameObject.FindGameObjectWithTag("Player");
+        m_Camera = FindObjectOfType<CameraController>().gameObject;
+
         if (GameManager.Instance.corruptionMeter >= 0 && GameManager.Instance.corruptionMeter < 20 && m_goodDirector.state != PlayState.Playing)
         {
+            m_Player.SetActive(false);
+            m_Camera.SetActive(false);
+
+            m_goodEnding.SetActive(true);
+
             PlayTimeLine(m_goodDirector);
-
-            //play Dialogue
         }
-        else if (GameManager.Instance.corruptionMeter >= 20 && GameManager.Instance.corruptionMeter < 40)
+        else if (GameManager.Instance.corruptionMeter >= 20 && GameManager.Instance.corruptionMeter < 40 && m_goodDirector.state != PlayState.Playing)
         {
+            m_Player.SetActive(false);
+            m_Camera.SetActive(false);
+
+            m_goodEnding.SetActive(true);
+
             PlayTimeLine(m_goodDirector);
-
-            //play Dialogue
         }
-        else if (GameManager.Instance.corruptionMeter >= 49 && GameManager.Instance.corruptionMeter < 60)
+        else if (GameManager.Instance.corruptionMeter >= 40 && GameManager.Instance.corruptionMeter < 60 && m_badDirector.state != PlayState.Playing)
         {
-            PlayTimeLine(m_badDirector);
+            m_Player.SetActive(false);
+            m_Camera.SetActive(false);
 
-            //play Dialogue
+            m_badEnding.SetActive(true);
+
+            PlayTimeLine(m_badDirector);
         }
-        else if (GameManager.Instance.corruptionMeter >= 60 && GameManager.Instance.corruptionMeter < 80)
+        else if (GameManager.Instance.corruptionMeter >= 60 && GameManager.Instance.corruptionMeter < 80 && m_badDirector.state != PlayState.Playing)
         {
-            PlayTimeLine(m_badDirector);
+            m_Player.SetActive(false);
+            m_Camera.SetActive(false);
 
-            //play Dialogue
+            m_badEnding.SetActive(true);
+
+            PlayTimeLine(m_badDirector);
         }
     }
 
@@ -43,7 +70,7 @@ public class TimeLineDirector : MonoBehaviour
         _director.Play();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
