1877d5b670eb54fec72ffc824c8979f5c850dacd
Tohwa | Sascha <Tohwa.gaming@gmail.com>
2024-08-07 12:41:33 +0200

    [ADDED] Camerazoom on Poster Interaction!!!!!

--- Echoes_of_Steel/Assets/Scripts/Collectibles/PosterInteractable.cs
+++ Echoes_of_Steel/Assets/Scripts/Collectibles/PosterInteractable.cs
@@ -1,15 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PosterInteractable : MonoBehaviour, IInteractable
 {
     [SerializeField] private InteractionStatus interactionStatus;
     [SerializeField] private string oneLiner;
+
+    private GameObject m_player;
+    private GameObject m_camera;
+    [SerializeField] private GameObject m_holoObj;
+
+    public float zoomDuration = 5.0f;
+
+    private Vector3 m_originalCameraPos;
+    private Quaternion m_originalCameraRota;
+    [SerializeField]private Transform m_cameraTarget;
+
+    public bool m_isInteracting;
+
     public void Interact()
     {
+        m_camera = GameObject.FindGameObjectWithTag("MainCamera");
+        m_player = GameObject.FindGameObjectWithTag("Player");
+
         interactionStatus.AddInteractedObject(gameObject.name);
         DialogueManager.instance.PlayOneLiner(oneLiner);
-        gameObject.SetActive(false);
+
+        StartCoroutine(ObservePoster());
+    }
+
+    private IEnumerator ObservePoster()
+    {
+        m_player.SetActive(false);
+        m_holoObj.SetActive(false);
+        m_originalCameraRota = m_camera.transform.rotation;
+        m_originalCameraPos = m_camera.transform.position;
+        Vector3 targetPos = m_cameraTarget.position;
+        m_camera.GetComponent<CameraController>().enabled = false;
+
+
+        float elapsedTime = 0;
+        float rotationSpeedFactor = 0.15f;
+
+        while (elapsedTime < zoomDuration)
+        {
+            m_camera.transform.position = Vector3.Lerp(m_player.transform.position, targetPos, elapsedTime / zoomDuration);
+            m_camera.transform.rotation = Quaternion.Lerp(m_camera.transform.rotation, m_cameraTarget.rotation, elapsedTime / zoomDuration * rotationSpeedFactor);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(5);
+
+        elapsedTime = 0;
+
+        while (elapsedTime < zoomDuration)
+        {
+            m_camera.transform.position = Vector3.Lerp(m_camera.transform.position, m_originalCameraPos, elapsedTime / zoomDuration);
+            m_camera.transform.rotation = Quaternion.Lerp(m_camera.transform.rotation, m_originalCameraRota, elapsedTime / zoomDuration * rotationSpeedFactor);
+            elapsedTime += Time.deltaTime;
+
+            if(m_camera.transform.position == m_originalCameraPos && m_camera.transform.rotation == m_originalCameraRota)
+            {
+                m_player.SetActive(true);
+                m_holoObj.SetActive(true);
+                m_camera.GetComponent<CameraController>().enabled = true;
+            }
+            yield return null;
+        }
     }
 }
